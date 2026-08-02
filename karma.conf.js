4a8096628b70a8a5b632e60ac0021ca4c2c93955
smahungu <sosthene.mahungu@outlook.com>
2026-08-02 22:22:44 +0200

    chore: nettoyer le projet final

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/olympic-games-starter'),
+      dir: require('path').join(__dirname, './coverage/tele-sport'),
       subdir: '.',
       reporters: [
         { type: 'html' },
